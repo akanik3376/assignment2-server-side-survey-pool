2c327758b0e67f7e9166d9bbf913c25561537fc6
unknown <you@example.com>
2023-11-26 13:37:23 +0600

    get one data base on id for details page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -62,7 +62,9 @@ async function run() {
 
         // survey start hare  get survey by id
         app.get('/api/v1/survey/:id', async (req, res) => {
-            const result = await surveyCollection.find().toArray()
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await surveyCollection.findOne(query)
             res.send(result)
         })
 
